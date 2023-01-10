bd19ba8286b5dfe417c17fa11154f30e9015dacd
Kaito Yamazaki <kyamaza2@gw.nsw.co.jp>
2023-01-10 10:48:06 +0900

    custom auth

--- api/message/index.js
+++ api/message/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Hello from the API"
+        text: "Hello from the API!"
     });
     // context.log('JavaScript HTTP trigger function processed a request.');
 
