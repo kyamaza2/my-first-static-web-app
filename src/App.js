4b9cc352108490838f024ab366f14d16e63d1590
Kaito Yamazaki <kyamaza2@gw.nsw.co.jp>
2023-02-08 13:56:25 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   return (
     <>
       <div>{data}</div>
-      <div>{logindata}</div>
+      {/* <div>{logindata}</div> */}
       <p><a href="/.auth/login/aad">Login</a></p>
       <p><a href="/.auth/logout/">Logout</a></p>
     </>
