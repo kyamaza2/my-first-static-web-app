3f3a8c1bbcd0769260d9dd4310d7b7ff507eee3f
Kaito Yamazaki <kyamaza2@gw.nsw.co.jp>
2023-02-08 15:23:59 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState('');
+  const [logindata, setLogindata] = useState('');
 
   useEffect(() => {
     (async function () {
       const { text } = await( await fetch(`/api/message`)).json();
       setData(text);
+    })();
+  });
+
+  useEffect(() => {
+    (async () => {
+      const { clientPrincipal } = await( await fetch(`/.auth/me`)).json();
+      console.log(clientPrincipal);
+      setLogindata(clientPrincipal);
     })();
   });
 
