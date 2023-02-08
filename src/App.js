a0be1106bd77f21fa39845b7105aeb460e79e70f
Kaito Yamazaki <kyamaza2@gw.nsw.co.jp>
2023-02-08 15:11:57 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState('');
-  const [logindata, setLogindata] = useState('');
 
   useEffect(() => {
     (async function () {
@@ -16,7 +15,7 @@ function App() {
       <div>{data}</div>
       {/* <div>{logindata}</div> */}
       <p><a href="/.auth/login/aad">Login</a></p>
-      <p><a href="/.auth/logout/">Logou</a></p>
+      <p><a href="/.auth/logout/">Logout</a></p>
     </>
   );
 }
